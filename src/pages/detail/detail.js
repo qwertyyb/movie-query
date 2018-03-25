270061576654d6d0a4d43ddc24778dd7c6d04d1e
qwertyyb <qwertyyb@foxmail.com>
2018-03-25 21:56:33 +0800

    完善详情页面

--- src/pages/detail/detail.js
+++ src/pages/detail/detail.js
@@ -6,7 +6,13 @@ import './detail.css'
 export default class Detail extends React.Component {
   constructor(props) {
     super(props)
+
+    this.showFull = this.showFull.bind(this)
+    this.hideFull = this.hideFull.bind(this)
+
     this.state = {
+      fulldetail: false,
+      movieInfo: {}
     }
   }
   componentDidMount() {
@@ -15,19 +21,26 @@ export default class Detail extends React.Component {
   }
   async getDetail(id) {
     let res = await getMovieInfo(id)
-    console.log({...res})
-    this.setState({...res})
+    this.setState({movieInfo: res})
+  }
+  showFull(type) {
+    this.setState({['full'+type]: true})
+  }
+  hideFull(type) {
+    this.setState({['full'+type]: false})
   }
   render() {
-    if (!this.state.title) {
+    if (!this.state.movieInfo.title) {
       return null
     }
-    let thumb = this.state.images.large
-    let type = this.state.genres.join('/')
-    let time = this.state.durations.join('/')
-    let language = this.state.languages.join('/')
-    let rate = this.state.rating.average
-    let date = this.state.pubdates.join('/')
+    let movieInfo = this.state.movieInfo
+    let thumb = movieInfo.images.large
+    let type = movieInfo.genres.join('/')
+    let time = movieInfo.durations.join('/')
+    let language = movieInfo.languages.join('/')
+    let rate = movieInfo.rating.average
+    let date = movieInfo.pubdates.join('/')
+    let actors = movieInfo.casts
     return (
       <div className="detail-page">
         <div className="top-wrapper">
@@ -47,9 +60,31 @@ export default class Detail extends React.Component {
           </div>
         </div>
         <div className="detail-wrapper">
-          <div className="detail-item">
+          <div className={"detail-item " + (this.state.fulldetail?'full':'')}>
             <h3 className="detail-item__title">剧情梗概</h3>
-            <div className="detail-item__content">{this.state.summary}</div>
+            <div className="detail-item__content">{movieInfo.summary}</div>
+            <button onClick={()=>this.showFull('detail')} className="detail-item__btn more-btn">显示更多</button>
+            <button onClick={()=>this.hideFull('detail')} className="detail-item__btn summary-btn">隐藏更多</button>
+          </div>
+          <div className="detail-item">
+            <h3 className="detail-item__title">演员</h3>
+            <div className="detail-item__actors">
+              {actors.map(actor => (
+                <div key={actor.id} className="actor">
+                  <img src={actor.avatars.large} alt={actor.name}/>
+                  <h4 className="actor-name">{actor.name}</h4>
+                </div>))}
+            </div>
+          </div>
+          <div className="detail-item">
+            <h3 className="detail-item__title">剧照</h3>
+            <div className="detail-item__images">
+              {movieInfo.photos.map(photo => (
+                <div key={photo.id} className="img-container">
+                  <img src={photo.cover} alt={photo.alt}/>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div> 
